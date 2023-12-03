cf89656e2ab70973ab3f28dca27dac83f2c45a85
Pranay Gupta <pranayguptamain@gmail.com>
2023-12-03 21:18:42 +0530

    Add Cloudinary configuration and dependencies

---
+++ frontend/src/config/cloudinary.ts
@@ -0,0 +1,7 @@
+export const cloudinaryConfig = {
+  cloudName: 'thepranaygupta',
+  apiKey: '523796778362381',
+  apiSecret: 'gQ9F9kJxLwG8Sfps9Fqnotobta0',
+  uploadPreset: 'medibot',
+  uploadURL: 'https://api.cloudinary.com/v1_1/thepranaygupta/image/upload',
+};
