0a1c1b42a0721c1ca0b72c235040329beb4afbf0
Pranay Gupta <pranayguptamain@gmail.com>
2024-05-14 22:18:07 +0530

    feat: Update Navbar component to include height for inline-flex container

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -27,7 +27,7 @@ export default function Navbar() {
   return (
     <div className='relative w-full bg-white'>
       <div className='mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8'>
-        <div className='inline-flex items-center space-x-2'>
+        <div className='inline-flex h-10 items-center space-x-2'>
           <span>
             <BrainCircuit />
           </span>
@@ -44,12 +44,12 @@ export default function Navbar() {
             ))}
           </ul>
         </div>
-        <div className='hidden lg:block'>
-          <button
+        <div className='hidden lg:block px-3 py-2'>
+          {/* <button
             type='button'
             className='rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black'>
             Button text
-          </button>
+          </button> */}
         </div>
         <div className='lg:hidden'>
           <Menu onClick={toggleMenu} className='h-6 w-6 cursor-pointer' />
